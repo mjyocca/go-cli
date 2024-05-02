c0b07e48f2484e828566e1a1c920af026d1606cf
mjyocca <mjyocca@gmail.com>
2024-05-02 11:52:11 -0700

    feat: add first command execution

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"example.com/go-cli/internal/commands/root"
+	"example.com/go-cli/internal/pkg/cmd"
+	"example.com/go-cli/version"
+	cli "github.com/hashicorp/cli"
 )
 
 func main() {
-	fmt.Fprintln(os.Stdout, []any{"Hello World"}...)
+	os.Exit(realMain())
+}
+
+func realMain() int {
+	args := os.Args[1:]
+
+	// setup root command
+	rootCmd := root.NewRootCmd()
+	cmdMap := cmd.BuildCmdMap(rootCmd)
+
+	cliRunner := cli.CLI{
+		Args:     args,
+		Commands: cmdMap,
+		Version:  version.GetVersion(),
+	}
+
+	// execute command
+	exitCode, err := cliRunner.Run()
+	if err != nil {
+		return 1
+	}
+	return exitCode
 }
