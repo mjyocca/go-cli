de7ccd28cbe7576889f7b129a2c538aeef9b2325
mjyocca <mjyocca@gmail.com>
2024-05-02 11:51:48 -0700

    chore: add generic placeholder root cmd

---
+++ internal/commands/root/root.go
@@ -0,0 +1,22 @@
+package root
+
+import (
+	"fmt"
+
+	"example.com/go-cli/internal/pkg/cmd"
+)
+
+func NewRootCmd() *cmd.Command {
+	cmd := &cmd.Command{
+		Name:  "root",
+		Short: "<help-short>",
+		Long:  "<help-long>",
+		Execute: func(cmd *cmd.Command, args []string) error {
+			fmt.Println("hello world!")
+			return nil
+		},
+		// TODO build subcommands
+	}
+
+	return cmd
+}
